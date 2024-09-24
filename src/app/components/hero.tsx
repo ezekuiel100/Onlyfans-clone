1788783df77fda8f8351cb9a7b0651a24416846b
unknown <ezekuiel100@gmail.com>
2024-09-24 15:06:27 -0300

    first commit

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <div className='grid grid-cols-2'>
+      <div className='bg-blue-400 h-screen '>
+        <div className='w-80 mt-44 ml-[30rem]'>
+          <img src='onlyfans.png' className='w-60 mb-3'></img>
+          <h2 className='text-3xl text-white'>
+            Inscreva-se para apoiar seus criadores favoritos
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
