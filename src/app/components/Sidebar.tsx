f74b8d84e39dec305c9fcd0610e50d96bc2a442c
unknown <ezekuiel100@gmail.com>
2024-09-30 15:40:46 -0300

    Sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -3,24 +3,25 @@ import {
   Cog6ToothIcon,
   HomeIcon,
 } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <nav className='flex flex-col gap-3 p-3 bg-green-300 w-48 h-screen'>
-      <a href='#' className='flex gap-1'>
+    <nav className='inline-block space-y-3 p-3 lg:w-1/5 h-screen border-r sticky'>
+      <Link href='#' className='flex gap-1 font-bold'>
         <HomeIcon className='size-6' />
-        Home
-      </a>
+        <span className='hidden lg:block'>Home</span>
+      </Link>
 
-      <a href='#' className='flex gap-1'>
+      <Link href='#' className='flex gap-1 font-bold'>
         <ClipboardIcon className='size-6' />
-        Dashboard
-      </a>
+        <span className='hidden lg:block'>Dashboard</span>
+      </Link>
 
-      <a href='#' className='flex gap-1'>
+      <Link href='#' className='flex gap-1 font-bold'>
         <Cog6ToothIcon className='size-6' />
-        Settings
-      </a>
+        <span className='hidden lg:block'>Settings</span>
+      </Link>
     </nav>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,14 @@ export default function Home() {
           </div>
         </header>
 
-        <Sidebar />
+        <div className='flex max-w-2xl lg:max-w-4xl bg-red-300 mx-auto'>
+          <Sidebar />
+          <div className='bg-green-300 w-full '>Feed</div>
+        </div>
       </SignedIn>
 
       <SignedOut>
-        <div className='grid grid-cols-2'>
+        <div className='grid grid-cols-2 justify-center items-center'>
           <Hero />
           <div className=' m-auto'>
             <SignIn routing='hash' />
