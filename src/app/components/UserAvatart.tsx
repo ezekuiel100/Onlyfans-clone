2bb7494c79314af7c71d653808230bd3d034606d
unknown <ezekuiel100@gmail.com>
2024-10-01 15:24:47 -0300

    User avatar

--- src/app/components/UserAvatart.tsx
+++ src/app/components/UserAvatart.tsx
@@ -6,12 +6,10 @@ export default function UserAvatar() {
   const { user } = useUser();
 
   return (
-    <>
-      <img
-        src={user?.imageUrl}
-        alt=''
-        className='rounded-full w-20 h-20 relative -top-7 left-5 border-2 select-none'
-      />
-    </>
+    <img
+      src={user?.imageUrl}
+      alt=''
+      className='rounded-full w-20 h-20  border-2 select-none '
+    />
   );
 }
--- src/app/components/userProfile.tsx
+++ src/app/components/userProfile.tsx
@@ -7,36 +7,40 @@ import UserAvatar from "./UserAvatart";
 
 export default function UserProfile() {
   return (
-    <div className='w-full h-44 bg-blue-300 relative'>
-      <img
-        src='paisagem.webp'
-        className='h-44 w-full object-cover select-none'
-      ></img>
+    <>
+      <div className='w-full h-44 bg-blue-300 relative'>
+        <img
+          src='paisagem.webp'
+          className='h-44 w-full object-cover select-none'
+        ></img>
 
-      <div className='absolute top-0 bg-black/20  w-full h-full'></div>
+        <div className='absolute top-0 bg-black/20  w-full h-full'></div>
 
-      <div className='absolute top-0 p-1 z-20 select-none'>
-        <p className='text-white text-xl font-semibold'>Jhon harris</p>
+        <div className='absolute top-0 p-1 select-none'>
+          <p className='text-white text-xl font-semibold'>Jhon harris</p>
 
-        <div className='flex gap-2'>
-          <div className='flex gap-1 text-white'>
-            <PhotoIcon className='size-6' />
-            35
-          </div>
+          <div className='flex gap-2'>
+            <div className='flex gap-1 text-white'>
+              <PhotoIcon className='size-6' />
+              35
+            </div>
 
-          <div className='flex gap-1 text-white'>
-            <VideoCameraIcon className='size-6' />
-            20
-          </div>
+            <div className='flex gap-1 text-white'>
+              <VideoCameraIcon className='size-6' />
+              20
+            </div>
 
-          <div className='flex gap-1 text-white'>
-            <HeartIcon className='size-6 ' />
-            540
+            <div className='flex gap-1 text-white'>
+              <HeartIcon className='size-6 ' />
+              540
+            </div>
           </div>
         </div>
-      </div>
 
-      <UserAvatar />
-    </div>
+        <div className='relative inline-block -mt-7 ml-3 z-50'>
+          <UserAvatar />
+        </div>
+      </div>
+    </>
   );
 }
