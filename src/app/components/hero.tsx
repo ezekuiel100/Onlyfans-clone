f2faf81077ca529d8d051451ff31642a3620ddfb
unknown <ezekuiel100@gmail.com>
2024-10-17 11:27:48 -0300

    Hero section

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -2,8 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Hero() {
   return (
-    <div className="h-screen grid md:grid-cols-2 ">
-      <div className="hidden md:grid grid-rows-2 bg-blue-400 h-screen items-center justify-end px-7 lg:px-28">
+    <div className="h-screen grid md:grid-cols-2  ">
+      <div className="hidden md:grid grid-rows-2 bg-blue-400 h-screen place-items-center lg:justify-end lg:px-28">
         <div className="w-80 mx-auto">
           <img src="onlyfans.png" className="w-60 mb-3 select-none"></img>
 
@@ -13,7 +13,7 @@ export default function Hero() {
         </div>
       </div>
 
-      <div className="m-auto ">
+      <div className="m-auto">
         <SignIn routing="hash" />
       </div>
     </div>
