ba7737a2a8d5350d7a243e240071e4d44f666245
unknown <ezekuiel100@gmail.com>
2024-11-01 17:44:27 -0300

    Text input

--- src/app/components/TextInput.tsx
+++ src/app/components/TextInput.tsx
@@ -1,10 +1,29 @@
-export default function TextInput({ label }) {
+"use client";
+
+import { useState } from "react";
+
+export default function TextInput({ label }: { label: string }) {
+  const [isFocused, setIsfocused] = useState(false);
+  const [value, setValue] = useState("");
+
+  const handleFocus = () => setIsfocused(true);
+  const handleBlur = () => setIsfocused(false);
+
   return (
-    <div>
-      <span className="text-sm text-gray-400">{label}</span>
+    <div className="pt-4 relative">
+      {(isFocused || value) && (
+        <span className="text-sm text-gray-400 absolute top-1 left-4 bg-white px-1">
+          {label}
+        </span>
+      )}
       <input
         type="text"
-        className="border border-gray-400 w-full p-2 rounded-md outline-none hover:border-gray-500 transition-all focus:border-blue-300"
+        value={value}
+        className="border border-gray-400 w-full p-3 rounded-md outline-none hover:border-gray-500 transition-all focus:border-blue-300"
+        placeholder={isFocused ? "" : label}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onChange={(e) => setValue(e.target.value)}
       />
     </div>
   );
